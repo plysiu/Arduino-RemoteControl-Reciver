18b000a9c0bd3fc78459434f7d2eeeb43564d7c9
Przemysław Płyś <przemyslaw.plys@gmail.com>
2015-10-21 00:06:20 +0200

    Got signal from transmiter using VirtualWire library

---
+++ RemoteControlReciver.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _RemoteControlReciver_H_
+#define _RemoteControlReciver_H_
+#include "Arduino.h"
+//add your includes for the project RemoteControlReciver here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project RemoteControlReciver here
+
+
+
+
+//Do not add code below this line
+#endif /* _RemoteControlReciver_H_ */
