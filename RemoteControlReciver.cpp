cb0f7653e73c0d881077fbbc7e4a1ac0114f407d
Przemysław Płyś <przemyslaw.plys@gmail.com>
2015-11-02 20:07:40 +0100

    First beta release

--- RemoteControlReciver.cpp
+++ RemoteControlReciver.cpp
@@ -1,51 +1,90 @@
 // Do not remove the include below
 #include "RemoteControlReciver.h"
-
-
-
-
+#include "StandardCplusplus.h"
+#include <RemoteControl.h>
 #include <VirtualWire.h>
+#include <vector>
 
 const int led_pin = 13;
 const int transmit_pin = 12;
 const int receive_pin = 10;
 const int transmit_en_pin = 3;
 
-void setup()
-{
-    delay(1000);
-    Serial.begin(9600);	// Debugging only
-    Serial.println("setup");
+void setup() {
+	Serial.begin(9600);
 
-    // Initialise the IO and ISR
-    vw_set_tx_pin(transmit_pin);
-    vw_set_rx_pin(receive_pin);
-    vw_set_ptt_pin(transmit_en_pin);
-    vw_set_ptt_inverted(true); // Required for DR3100
-    vw_setup(2000);	 // Bits per sec
+	vw_set_tx_pin(transmit_pin);
+	vw_set_rx_pin(receive_pin);
+	vw_set_ptt_pin(transmit_en_pin);
+	vw_set_ptt_inverted(true);
+	vw_setup(12000);
+	vw_rx_start();
 
-    vw_rx_start();       // Start the receiver PLL running
-
-    pinMode(led_pin, OUTPUT);
+	pinMode(led_pin, OUTPUT);
 }
+std::vector<RemoteControl::Token> recive() {
+	std::vector<RemoteControl::Token> tokens;
+	uint8_t buf[VW_MAX_MESSAGE_LEN];
+	uint8_t buflen = VW_MAX_MESSAGE_LEN;
 
-void loop()
-{
-    uint8_t buf[VW_MAX_MESSAGE_LEN];
-    uint8_t buflen = VW_MAX_MESSAGE_LEN;
-
-    if (vw_get_message(buf, &buflen)) // Non-blocking
-    {
-	int i;
-
-        digitalWrite(led_pin, HIGH); // Flash a light to show received good message
-	// Message with a good checksum received, dump it.
-
-	for (i = 0; i < buflen; i++)
-	{
-	    Serial.print((char) buf[i]);
+	if (vw_get_message(buf, &buflen)) {
+		digitalWrite(led_pin, HIGH);
+		RemoteControl::Tokenizer tokenizer((char*) buf);
+		for (RemoteControl::Token token = tokenizer.next();
+				token.type != RemoteControl::TokenType::End;
+				token = tokenizer.next()) {
+			tokens.insert(tokens.begin(), token);
+		}
 	}
-	Serial.println();
-        digitalWrite(led_pin, LOW);
-    }
+	digitalWrite(led_pin, LOW);
+	return tokens;
+}
+std::vector<RemoteControl::Signal> signals;
+void loop() {
+	signals = RemoteControl::transformTokensToSignal(recive());
+	if (signals.size() > 0) {
+		for (unsigned int i = 0; i < signals.size(); i++) {
+
+			switch (signals[i].id) {
+			case 0:
+				Serial.print("Signal: ");
+				Serial.print(signals[i].id);
+				Serial.print(" Value: ");
+				Serial.println(signals[i].value);
+				break;
+			case 1:
+				Serial.print("Signal: ");
+				Serial.print(signals[i].id);
+				Serial.print(" Value: ");
+				Serial.println(signals[i].value);
+				break;
+			case 2:
+				Serial.print("Signal: ");
+				Serial.print(signals[i].id);
+				Serial.print(" Value: ");
+				Serial.println(signals[i].value);
+				break;
+			case 3:
+				Serial.print("Signal: ");
+				Serial.print(signals[i].id);
+				Serial.print(" Value: ");
+				Serial.println(signals[i].value);
+				break;
+
+//			case 4:
+//				Serial.print("Signal: ");
+//				Serial.print(signals[i].id);
+//				Serial.print(" Value: ");
+//				Serial.println(signals[i].value);
+//				break;
+//
+//			case 5:
+//				Serial.print("Signal: ");
+//				Serial.print(signals[i].id);
+//				Serial.print(" Value: ");
+//				Serial.println(signals[i].value);
+//				break;
+			}
+		}
+	}
 }
